af652ae05220189ed880b946e3975587105eab07
Brian <brian.bruno@outlook.com>
2019-11-19 19:45:11 -0300

    classe grafo

---
+++ src/Grafo.java
@@ -0,0 +1,37 @@
+public class Grafo {
+
+    boolean isadjacente (Vertice v1, Vertice v2) { return false; }
+
+    int getGrau (Vertice v1) { return false; }
+
+    boolean isRegular (Grafo G) { return false; }
+
+    boolean isIsolado (Vertice v1) { return false; }
+
+    boolean isPendente (Vertice v1) { return false; }
+
+    boolean isNulo (Grafo G) { return false; }
+
+    boolean isCompleto (Grafo G) { return false; }
+
+    boolean isConexo (Grafo G) { return false; }
+
+    boolean isBipartido (Grafo G) { return false; }
+
+    Grafo getComplementar (Grafo G) { return false; }
+
+    boolean isEuleriano (Grafo G ) { return false; }
+
+    boolean isUnicursal (Grafo G ) { return false; }
+
+    boolean hasCiclo (Grafo G) { return false; }
+
+    int getGrauEntrada (Vertice v1) { return false; }
+
+    void ordenacaoTopologica (Grafo G) { return false; } //verifique se o grafo é acíclico antes
+
+    Grafo getTransposto (Grafo G) { return false; }
+
+    boolean isFConexo (Grafo G) { return false; }
+
+}
